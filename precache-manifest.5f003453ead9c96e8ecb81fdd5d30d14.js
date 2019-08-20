55aabcd9c01537543bd8f41935929cda62d8e599
Parth Raval <parthraval@thinkbiz.co.in>
2019-08-20 18:13:32 +0530

    Updates

---
+++ precache-manifest.5f003453ead9c96e8ecb81fdd5d30d14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37afd2c5b4287b2b240fbd77ffe061a5",
+    "url": "/react-pages/index.html"
+  },
+  {
+    "revision": "6ede448eada618504941",
+    "url": "/react-pages/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "042900448c8ec5facc1e",
+    "url": "/react-pages/static/js/2.1480c97c.chunk.js"
+  },
+  {
+    "revision": "6ede448eada618504941",
+    "url": "/react-pages/static/js/main.8a9c3caa.chunk.js"
+  },
+  {
+    "revision": "a2fda37432adc8a7dd4c",
+    "url": "/react-pages/static/js/runtime~main.0f5aa2db.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
